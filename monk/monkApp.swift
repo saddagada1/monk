723a61f0245ccb216d6d0eb124e4cf5dacda62a4
saddagada1 <saddagada1@gmail.com>
2024-01-09 22:47:43 -0500

    Initial Commit

---
+++ monk/monkApp.swift
@@ -0,0 +1,17 @@
+//
+//  monkApp.swift
+//  monk
+//
+//  Created by flighty on 2024-01-09.
+//
+
+import SwiftUI
+
+@main
+struct monkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
